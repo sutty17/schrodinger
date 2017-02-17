33f3235600a9eddb1b6c6aa62d00580039fb065d
Thom Sutcliffe <t.sutcliffe@ocado.com>
2017-02-17 17:43:48 +0000

    Started work on making Schrodinger<T> implement Comparable<Schrodinger<T>>.

--- src/main/java/com/lawrenceweetman/schrodinger/Schrodinger.java
+++ src/main/java/com/lawrenceweetman/schrodinger/Schrodinger.java
@@ -1,8 +1,17 @@
 package com.lawrenceweetman.schrodinger;
 
+import java.util.Comparator;
 import java.util.Optional;
+import java.util.function.Function;
 
-public class Schrodinger<T> {
+public class Schrodinger<T> implements Comparable<Schrodinger<T>> {
+    private final Function<Comparator<T>, Comparator<Schrodinger<T>>> schrodingerComparatorFactory = delegateComparator -> (o1, o2) -> {
+        if (o1.is(o2)) {
+            return 0;
+        } else {
+            return delegateComparator.compare(o1.value.orElse(null), o2.value.orElse(null));
+        }
+    };
 
     Optional<T> value = Optional.empty();
 
@@ -45,5 +54,16 @@ public class Schrodinger<T> {
     @Override
     public int hashCode() {
         return value != null ? value.hashCode() : 0;
+    }
+
+    @Override
+    public int compareTo(Schrodinger<T> o) {
+        if(is(o)) {
+            return 0;
+        } else if (o.value.orElse(null) instanceof Comparable) {
+            return ((Comparable<T>) value.get()).compareTo(o.value.get());
+        } else {
+            throw new UnsupportedOperationException("Cannot compare incomparable values. That would be silly.");
+        }
     }
 }
